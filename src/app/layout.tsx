128d129360982bf025b432a2322b1a32b4c9ff2d
Shamilx <shamil.shamiyevx@gmail.com>
2024-04-15 18:43:45 +0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 const inter = Inter({ subsets: ["latin"] });
+
+// Next
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react"
 
 // Navbar
 import QuizzersNavbar from "@/components/lib/navbar/QuizzersNavbar";
@@ -29,6 +32,7 @@ export default function RootLayout({
         </AuthProvider>
         
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
